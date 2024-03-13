ab4752c90fc51be880c1a00b69773377598cb74a
Tnalxmsk <alstnwkd990@naver.com>
2024-03-13 15:00:40 +0900

    feat: my-pageFragment 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.mypage
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentMyPageBinding
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    override fun setLayout() {
+
+    }
+}

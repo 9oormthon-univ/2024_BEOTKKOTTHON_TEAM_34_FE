4810c0b29fb89ce1ccb116de99ceb6cf93fb4b1c
Tnalxmsk <alstnwkd990@naver.com>
2024-03-13 14:59:50 +0900

    feat: 채팅화면 ChattingFragment 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/chatting/ChattingMainFragment.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.chatting
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentChattingMainBinding
+
+class ChattingMainFragment : BaseFragment<FragmentChattingMainBinding>(R.layout.fragment_chatting_main) {
+
+    override fun setLayout() {
+
+    }
+}

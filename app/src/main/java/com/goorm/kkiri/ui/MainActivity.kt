19587a60930191c9e0e80c2bad1d65477c9fbb48
Tnalxmsk <alstnwkd990@naver.com>
2024-03-13 15:00:57 +0900

    feat: Navigation Graph 기능 구현

---
+++ app/src/main/java/com/goorm/kkiri/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.goorm.kkiri.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseActivity
+import com.goorm.kkiri.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun setLayout() {
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val bottomNavigationView = binding.bottomNavigationMain
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
+    }
+}

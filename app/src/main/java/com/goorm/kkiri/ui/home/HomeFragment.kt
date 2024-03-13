0426b0c3e9643b970b7c7ac0817ae07350e3cb97
Tnalxmsk <alstnwkd990@naver.com>
2024-03-13 14:58:38 +0900

    feat: Navigation Graph 구현

---
+++ app/src/main/java/com/goorm/kkiri/ui/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.home
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun setLayout() {
+
+    }
+}

9e073c94aa023ac79d9f6c8c9b222918d9e46a48
jiwon <pjw99032986@gmail.com>
2024-03-15 05:07:34 +0900

    feat: HelpedListFragment 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/fragment/HelpedListFragment.kt
@@ -0,0 +1,8 @@
+package com.goorm.kkiri.ui.mypage.fragment
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentHelpedListBinding
+class HelpedListFragment : BaseFragment<FragmentHelpedListBinding>(R.layout.fragment_helped_list) {
+    override fun setLayout() {
+    }
+}

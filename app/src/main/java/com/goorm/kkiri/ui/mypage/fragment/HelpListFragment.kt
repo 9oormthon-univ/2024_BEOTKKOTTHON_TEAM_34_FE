41aa326187cb3b8ccd298e7ca43155b3211b679d
jiwon <pjw99032986@gmail.com>
2024-03-15 05:07:42 +0900

    feat: HelpListFragment 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/fragment/HelpListFragment.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.mypage.fragment
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentHelpListBinding
+
+class HelpListFragment : BaseFragment<FragmentHelpListBinding>(R.layout.fragment_help_list) {
+
+    override fun setLayout() {
+
+    }
+}

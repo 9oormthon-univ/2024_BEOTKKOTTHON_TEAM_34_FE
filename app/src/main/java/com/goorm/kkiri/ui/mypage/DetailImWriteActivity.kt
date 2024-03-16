fd53cf9148fbccf9f40e5f59ec7831bc6ea0ed22
jiwon <pjw99032986@gmail.com>
2024-03-16 16:34:15 +0900

    feat : DetailImWriteActivity.kt 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/DetailImWriteActivity.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.mypage
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseActivity
+import com.goorm.kkiri.databinding.ActivityDetailImWriteBinding
+
+class DetailImWriteActivity : BaseActivity<ActivityDetailImWriteBinding>(R.layout.activity_detail_im_write) {
+    override fun setLayout() {
+
+    }
+
+}

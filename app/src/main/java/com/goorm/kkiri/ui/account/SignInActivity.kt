41772c52eedc6d34209d6c4e2608e3730111077c
jiwon <pjw99032986@gmail.com>
2024-03-16 20:40:21 +0900

    feat : 로그인 화면 추가

---
+++ app/src/main/java/com/goorm/kkiri/ui/account/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.goorm.kkiri.ui.account
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseActivity
+import com.goorm.kkiri.databinding.ActivitySignInBinding
+
+class SignInActivity : BaseActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    override fun setLayout() {
+
+    }
+
+}

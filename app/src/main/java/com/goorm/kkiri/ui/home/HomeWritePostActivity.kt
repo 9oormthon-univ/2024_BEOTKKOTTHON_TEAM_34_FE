be4e1de520505f4b0093c201f3c0f7b691a15c1b
Tnalxmsk <alstnwkd990@naver.com>
2024-03-17 17:16:45 +0900

    feat: 게시글 작성 화면 구현

---
+++ app/src/main/java/com/goorm/kkiri/ui/home/HomeWritePostActivity.kt
@@ -0,0 +1,12 @@
+package com.goorm.kkiri.ui.home
+
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseActivity
+import com.goorm.kkiri.databinding.ActivityHomeWritePostBinding
+
+class HomeWritePostActivity : BaseActivity<ActivityHomeWritePostBinding>(R.layout.activity_home_write_post) {
+
+    override fun setLayout() {
+
+    }
+}

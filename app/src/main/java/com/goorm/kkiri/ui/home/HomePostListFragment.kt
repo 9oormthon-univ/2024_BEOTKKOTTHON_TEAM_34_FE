bfd7fce6c852a4d786159d9d903dbed83b2e4285
Tnalxmsk <alstnwkd990@naver.com>
2024-03-17 20:08:53 +0900

    feat: PopupMenu 및 화면 이동 구현

--- app/src/main/java/com/goorm/kkiri/ui/home/HomePostListFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/home/HomePostListFragment.kt
@@ -1,11 +1,17 @@
 package com.goorm.kkiri.ui.home
 
+import android.view.ContextThemeWrapper
+import androidx.appcompat.widget.PopupMenu
 import androidx.navigation.fragment.findNavController
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
 import com.goorm.kkiri.databinding.FragmentHomePostListBinding
+import com.goorm.kkiri.ui.common.PostType
+import com.goorm.kkiri.ui.common.PostType.*
 
 class HomePostListFragment : BaseFragment<FragmentHomePostListBinding>(R.layout.fragment_home_post_list) {
+    private var postType: PostType? = null
+
     override fun setLayout() {
         setClickListener()
     }
@@ -14,6 +20,31 @@ class HomePostListFragment : BaseFragment<FragmentHomePostListBinding>(R.layout.
         with(binding) {
             toolbarHomePostList.setNavigationOnClickListener {
                 findNavController().navigateUp()
+            }
+            ibPostWriteAddButton.setOnClickListener {
+                val contentWThemeWrapper = ContextThemeWrapper(requireContext(), R.style.PopupMenuStyle)
+
+                val popupMenu = PopupMenu(contentWThemeWrapper, it)
+                popupMenu.menuInflater.inflate(R.menu.post_type_menu, popupMenu.menu)
+                popupMenu.setForceShowIcon(true)
+                popupMenu.show()
+                popupMenu.setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.menu_help_me -> {
+                            postType =  HelpMe
+                            val action = HomePostListFragmentDirections.actionHomePostListToHomeWritePost(postType!!)
+                            findNavController().navigate(action)
+                            return@setOnMenuItemClickListener true
+                        }
+                        R.id.menu_help_you -> {
+                            postType = HelpYou
+                            val action = HomePostListFragmentDirections.actionHomePostListToHomeWritePost(postType!!)
+                            findNavController().navigate(action)
+                            return@setOnMenuItemClickListener true
+                        }
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                }
             }
         }
     }

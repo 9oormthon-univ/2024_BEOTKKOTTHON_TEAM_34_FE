e59087ed215934547a1cc8e0be64204ea9ade16a
Tnalxmsk <alstnwkd990@naver.com>
2024-03-17 20:09:24 +0900

    feat: 게시글 작성 화면 버튼 기능 구현
    뒤로가기, 작성 완료 시 화면 종료 기능 구현

--- app/src/main/java/com/goorm/kkiri/ui/home/HomeWritePostActivity.kt
+++ app/src/main/java/com/goorm/kkiri/ui/home/HomeWritePostActivity.kt
@@ -1,12 +1,27 @@
 package com.goorm.kkiri.ui.home
 
+import androidx.navigation.navArgs
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseActivity
 import com.goorm.kkiri.databinding.ActivityHomeWritePostBinding
 
 class HomeWritePostActivity : BaseActivity<ActivityHomeWritePostBinding>(R.layout.activity_home_write_post) {
+    private val args by navArgs<HomeWritePostActivityArgs>()
 
     override fun setLayout() {
+        setClickBackButton()
+        setWriteCompleteButton()
+    }
 
+    private fun setClickBackButton() {
+        binding.toolbarHomeWritePost.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun setWriteCompleteButton() {
+        binding.btnPostWriteCompleteButton.setOnClickListener {
+            finish()
+        }
     }
 }

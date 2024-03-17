0ac3bd9267f39e28468c456d878ac94eb49bf98e
jiwon <pjw99032986@gmail.com>
2024-03-17 20:36:27 +0900

    feat : 로그인 프래그먼트 구현 (오류처리, 포커스 적용)

--- app/src/main/java/com/goorm/kkiri/ui/account/RegisterFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/account/RegisterFragment.kt
@@ -9,7 +9,6 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.goorm.kkiri.R

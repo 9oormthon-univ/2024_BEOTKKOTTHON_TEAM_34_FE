d50a8e3ac0eeba63b6d85d0915881110583cd690
jiwon <pjw99032986@gmail.com>
2024-03-19 05:10:33 +0900

    feat : 기본 url , 이미지 바인딩

---
+++ app/src/main/java/com/goorm/kkiri/util/BindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.goorm.kkiri.util
+
+import android.net.Uri
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.goorm.kkiri.R
+
+object BindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("imageUri")
+    fun setImageUri(view: ImageView, uri: Uri?) {
+        if (uri != null) {
+            view.setImageURI(uri)
+        } else {
+            // URI가 null일 때 기본 이미지 설정 (임시)
+            view.setImageResource(R.mipmap.image_iw_sample)
+        }
+    }
+}

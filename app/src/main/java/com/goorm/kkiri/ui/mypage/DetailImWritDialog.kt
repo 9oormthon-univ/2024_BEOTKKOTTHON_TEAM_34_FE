304eecde4c889cd02c3b58251e929775b7407919
jiwon <pjw99032986@gmail.com>
2024-03-19 05:11:15 +0900

    refactor : 다이얼로그 크기 부모뷰에 맞추는 코드 추가

--- app/src/main/java/com/goorm/kkiri/ui/mypage/DetailImWritDialog.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/DetailImWritDialog.kt
@@ -22,6 +22,15 @@ class DetailImWritDialog(
     private var _binding: DialogMyWrittenEditRemoveBinding? = null
     private val binding get() = _binding!!
 
+    //다이얼로그의 크기를 부모의 뷰에 맞추기위한 세팅
+    override fun onStart() {
+        super.onStart()
+        val window = dialog?.window
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        window?.setLayout(width, height) // 다이얼로그의 너비와 높이 설정
+        window?.setGravity(Gravity.BOTTOM) // 필요한 경우 다이얼로그의 위치 조정
+    }
 
     //다이얼로그 바인딩
     override fun onCreateView(
@@ -40,7 +49,6 @@ class DetailImWritDialog(
         dialog?.apply {
             requestWindowFeature(Window.FEATURE_NO_TITLE)
             window?.apply {
-
                 //백그라운드 투명
                 setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
                 //너비는 match_parent, 높이는 wrap_content

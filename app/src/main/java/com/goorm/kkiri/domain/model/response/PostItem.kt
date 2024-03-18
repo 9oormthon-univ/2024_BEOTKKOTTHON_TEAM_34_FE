e6bbe2e2d051e9bedf10697fa929c00335b47a77
Tnalxmsk <alstnwkd990@naver.com>
2024-03-18 16:28:22 +0900

    feat: 게시글 Data class 추가
    게시글 목록 화면 게시글 data class 구현

--- app/src/main/java/com/goorm/kkiri/domain/model/response/PostItem.kt
+++ app/src/main/java/com/goorm/kkiri/domain/model/response/PostItem.kt
@@ -3,7 +3,7 @@ package com.goorm.kkiri.domain.model.response
 data class PostItem(
     val postId: Long,
     val postTitle: String,
-    val imageUrl: String,
+    val imageUrl: Int?,
     val createAt: String,
     val beansCount: Int,
     val chattingCount: Int

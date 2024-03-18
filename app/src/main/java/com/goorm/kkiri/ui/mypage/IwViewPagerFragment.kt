cddc8e1ffa22622f37b558fc9f1d3bf11c6aabcf
jiwon <pjw99032986@gmail.com>
2024-03-18 17:06:49 +0900

    feat : 프레그먼트에 들어갈 이미지 파일 소스 코드

---
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/IwViewPagerFragment.kt
@@ -0,0 +1,30 @@
+package com.goorm.kkiri.ui.mypage
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.goorm.kkiri.R
+import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.databinding.FragmentIwViewPagerBinding
+
+class IwViewPagerFragment : BaseFragment<FragmentIwViewPagerBinding>(R.layout.fragment_iw_view_pager) {
+
+    override fun setLayout() {
+        arguments?.let {
+            val imgDrawableResId = it.getInt(ARG_IMG_DRAWABLE)
+            val drawable = ContextCompat.getDrawable(requireContext(), imgDrawableResId)
+            binding.ivIwImage.setImageDrawable(drawable)
+        }
+    }
+
+    companion object {
+        private const val ARG_IMG_DRAWABLE = "imageDrawable"
+
+        fun newInstance(imgDrawableResId: Int): IwViewPagerFragment {
+            val fragment = IwViewPagerFragment()
+            val args = Bundle().apply {
+                putInt(ARG_IMG_DRAWABLE, imgDrawableResId)
+            }
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}

f333d892f128f0793cbc28e33f7704d5e2197eca
Tnalxmsk <alstnwkd990@naver.com>
2024-03-18 16:28:51 +0900

    feat: 게시글 목록 RecyclerView Adapter 구현

--- app/src/main/java/com/goorm/kkiri/ui/home/adapter/HomePostListAdapter.kt
+++ app/src/main/java/com/goorm/kkiri/ui/home/adapter/HomePostListAdapter.kt
@@ -2,8 +2,9 @@ package com.goorm.kkiri.ui.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.goorm.kkiri.databinding.ItemHomeHelpPostBinding
+import com.goorm.kkiri.databinding.ItemHelpPostBinding
 import com.goorm.kkiri.domain.model.response.PostItem
 
 class HomePostListAdapter : RecyclerView.Adapter<HomePostListAdapter.HomePostListViewHolder>() {
@@ -19,18 +20,46 @@ class HomePostListAdapter : RecyclerView.Adapter<HomePostListAdapter.HomePostLis
         holder.bind(postItems[position])
     }
 
+    fun update(newItems: List<PostItem>) {
+        val diffUtil = PostListDiffUtil(postItems, newItems)
+        val result = DiffUtil.calculateDiff(diffUtil)
+        postItems.clear()
+        postItems.addAll(newItems)
+        result.dispatchUpdatesTo(this)
+    }
+
+    class PostListDiffUtil(
+        private val oldItems: List<PostItem>,
+        private val newItems: List<PostItem>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldItems.size
+
+        override fun getNewListSize(): Int = newItems.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldItems[oldItemPosition]
+            val newItem = newItems[newItemPosition]
+            return oldItem.postId == newItem.postId
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldItems[oldItemPosition] == newItems[newItemPosition]
+        }
+
+    }
+
     class HomePostListViewHolder(
-        private val binding: ItemHomeHelpPostBinding
+        private val binding: ItemHelpPostBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(postItem: PostItem) {
-
+            binding.postItem = postItem
         }
 
         companion object {
             fun from(parent: ViewGroup): HomePostListViewHolder {
                 return HomePostListViewHolder(
-                    ItemHomeHelpPostBinding.inflate(
+                    ItemHelpPostBinding.inflate(
                         LayoutInflater.from(parent.context),
                         parent,
                         false

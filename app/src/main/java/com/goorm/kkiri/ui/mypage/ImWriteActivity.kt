a4160f565f4081db7af7e1dc518ed53fd92fd6f4
jiwon <pjw99032986@gmail.com>
2024-03-20 04:22:35 +0900

    refactor : viewModel 추가 및 리스트 추가, 삭제 버튼 (시험용) 추가

--- app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteActivity.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteActivity.kt
@@ -1,25 +1,39 @@
 package com.goorm.kkiri.ui.mypage
 
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModelProvider
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseActivity
 import com.goorm.kkiri.databinding.ActivityImWriteBinding
+import com.goorm.kkiri.ui.mypage.viewmodel.ImWriteViewModel
 
-
+@RequiresApi(Build.VERSION_CODES.O)
 class ImWriteActivity : BaseActivity<ActivityImWriteBinding>(R.layout.activity_im_write) {
+    private lateinit var viewModel: ImWriteViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun setLayout() {
+        viewModel = ViewModelProvider(this).get(ImWriteViewModel::class.java)
         setFragment()
         setOnClick()
+
     }
 
-    private fun setOnClick(){
-        with(binding){
+    private fun setOnClick() {
+        with(binding) {
             iwDtBt.setOnClickListener {
-                startActivity(Intent(this@ImWriteActivity, DetailImWriteActivity::class.java))
+                viewModel.removeFirstItem()
+            }
+
+            iwDtBt2.setOnClickListener {
+                viewModel.createFirstItem()
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setFragment(){
         with(binding) {
             supportFragmentManager.beginTransaction().apply {

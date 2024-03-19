dc34c2fa5cfa86850cf3593fe0bb25e7a1afca61
jiwon <pjw99032986@gmail.com>
2024-03-20 04:21:39 +0900

    refactor : viewModel 추가 및 Observer 적용

--- app/src/main/java/com/goorm/kkiri/ui/mypage/HelpedListFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/HelpedListFragment.kt
@@ -1,24 +1,34 @@
 package com.goorm.kkiri.ui.mypage
+
 import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.activityViewModels
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
 import com.goorm.kkiri.databinding.FragmentHelpedListBinding
 import com.goorm.kkiri.ui.mypage.adapter.MenuClickListener
 import com.goorm.kkiri.ui.mypage.adapter.MyWrittenHelpAdapter
+import com.goorm.kkiri.ui.mypage.viewmodel.ImWriteViewModel
 
-class HelpedListFragment : BaseFragment<FragmentHelpedListBinding>(R.layout.fragment_helped_list) ,
+
+@RequiresApi(Build.VERSION_CODES.O)
+class HelpedListFragment : BaseFragment<FragmentHelpedListBinding>(R.layout.fragment_helped_list),
     MenuClickListener {
-    @RequiresApi(Build.VERSION_CODES.O)
     private val myWrittenHelpedAdapter = MyWrittenHelpAdapter(this)
+    private val viewModel: ImWriteViewModel by activityViewModels()
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun setLayout() {
+
+        // ViewModel 초기화
         binding.recyclerHelped.adapter = myWrittenHelpedAdapter
+        viewModel.itemList.observe(viewLifecycleOwner) { items ->
+            // 어댑터에 데이터 업데이트
+            myWrittenHelpedAdapter.update(items)
+        }
     }
 
     override fun menuClickListener(position: Long) {
-        startActivity(Intent(requireContext() , DetailImWriteActivity::class.java))
+        startActivity(Intent(requireContext(), DetailImWriteActivity::class.java))
     }
 }

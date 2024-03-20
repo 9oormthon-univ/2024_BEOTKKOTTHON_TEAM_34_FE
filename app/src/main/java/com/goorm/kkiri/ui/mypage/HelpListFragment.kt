de1a5b8045e1c18407790e1bef21d9d904dd865c
jiwon <pjw99032986@gmail.com>
2024-03-20 16:52:54 +0900

    refactor : setLayout 코드 분리

--- app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
@@ -6,6 +6,7 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.data.local.DataSource
 import com.goorm.kkiri.databinding.FragmentHelpListBinding
 import com.goorm.kkiri.ui.mypage.adapter.MenuClickListener
 import com.goorm.kkiri.ui.mypage.adapter.MyWrittenHelpAdapter
@@ -15,8 +16,14 @@ import com.goorm.kkiri.ui.mypage.viewmodel.ImWriteViewModel
 class HelpListFragment : BaseFragment<FragmentHelpListBinding>(R.layout.fragment_help_list),
     MenuClickListener {
     private val myWrittenHelpAdapter = MyWrittenHelpAdapter(this)
+
     private val viewModel: ImWriteViewModel by activityViewModels()
     override fun setLayout() {
+        initAdapter()
+    }
+    private fun initAdapter(){
+        DataSource.initHelpMePostItems()
+        myWrittenHelpAdapter.update(DataSource.writtemItems.toList())
         binding.recyclerHelp.adapter = myWrittenHelpAdapter
         viewModel.itemList.observe(viewLifecycleOwner) { items ->
             // 어댑터에 데이터 업데이트

cde02cd8dbeea9c01be3c5b0149d3708960b47b5
jiwon <pjw99032986@gmail.com>
2024-03-21 05:05:29 +0900

    refactor : 프레그먼트 -> 액티비티 이동 전환

--- app/src/main/java/com/goorm/kkiri/ui/account/SignInFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/account/SignInFragment.kt
@@ -1,11 +1,12 @@
 package com.goorm.kkiri.ui.account
 
-import android.view.View
+import android.content.Intent
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
 import com.goorm.kkiri.databinding.FragmentSignInBinding
+import com.goorm.kkiri.ui.MainActivity
 
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
     override fun setLayout() {
@@ -20,7 +21,8 @@ class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sig
                 navigateToDestination(SignInFragmentDirections.actionNavigationSignInToNavigationRegister())
             }
             btLogin.setOnClickListener {
-                navigateToDestination(SignInFragmentDirections.actionNavigationSignInToNavigationHome())
+                startActivity(Intent(requireContext(), MainActivity::class.java))
+
             }
 
 

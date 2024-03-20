9083448f30c07f83e24cf2fb1e6e790780bcf1b5
jiwon <pjw99032986@gmail.com>
2024-03-20 19:28:26 +0900

    refactor : 함수 리팩터링 및 탑 버튼, 리사이클러뷰 어댑터 세팅

--- app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
@@ -2,8 +2,11 @@ package com.goorm.kkiri.ui.mypage
 
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
 import com.goorm.kkiri.data.local.DataSource
@@ -15,22 +18,56 @@ import com.goorm.kkiri.ui.mypage.viewmodel.ImWriteViewModel
 @RequiresApi(Build.VERSION_CODES.O)
 class HelpListFragment : BaseFragment<FragmentHelpListBinding>(R.layout.fragment_help_list),
     MenuClickListener {
-    private val myWrittenHelpAdapter = MyWrittenHelpAdapter(this)
 
     private val viewModel: ImWriteViewModel by activityViewModels()
+    private val myWrittenHelpAdapter = MyWrittenHelpAdapter(this)
+
+
     override fun setLayout() {
         initAdapter()
     }
-    private fun initAdapter(){
-        DataSource.initHelpMePostItems()
-        myWrittenHelpAdapter.update(DataSource.writtemItems.toList())
+
+    //어댑터 초기화
+    private fun initAdapter() {
+        setupRecyclerView()
+        observeViewModel()
+    }
+    //리사이클러뷰 세팅
+    private fun setupRecyclerView() {
         binding.recyclerHelp.adapter = myWrittenHelpAdapter
-        viewModel.itemList.observe(viewLifecycleOwner) { items ->
-            // 어댑터에 데이터 업데이트
-            myWrittenHelpAdapter.update(items)
-        }
     }
 
+    //뷰 모델 옵저버, 스크롤 이벤트 장착
+    private fun observeViewModel() {
+        viewModel.itemList.observe(viewLifecycleOwner) { items ->
+            myWrittenHelpAdapter.update(items)
+        }
+
+        viewModel.scrollToTopEvent.observe(viewLifecycleOwner) {
+            scrollToTop()
+        }
+
+        binding.recyclerHelp.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                handleScrollStateChange(newState)
+            }
+        })
+    }
+
+    //스크롤이 멈추면 버튼의 가시성 변경
+    private fun handleScrollStateChange(newState: Int) {
+        val recyclerView = binding.recyclerHelp
+        val canScrollUp = recyclerView.canScrollVertically(-1)
+        viewModel.setFloatButtonVisibility(canScrollUp || newState != RecyclerView.SCROLL_STATE_IDLE)
+    }
+
+    //스크롤을 맨 위로 올리는 함수
+    private fun scrollToTop() {
+        binding.recyclerHelp.smoothScrollToPosition(0)
+    }
+
+    //메뉴를 누르면 이동
     override fun menuClickListener(position: Long) {
         startActivity(Intent(requireContext(), DetailImWriteActivity::class.java))
     }

d931baed20af78d152ec7986d0e2438304425945
jiwon <pjw99032986@gmail.com>
2024-03-20 19:41:25 +0900

    refactor : 함수 리팩터링 및 탑 버튼, 리사이클러뷰 어댑터 세팅

--- app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteFragment.kt
@@ -3,11 +3,14 @@ package com.goorm.kkiri.ui.mypage
 import android.graphics.Typeface
 import android.os.Build
 import android.view.Gravity
+import android.view.View
+import android.view.animation.AlphaAnimation
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
@@ -15,43 +18,61 @@ import com.goorm.kkiri.databinding.FragmentImWriteBinding
 import com.goorm.kkiri.ui.mypage.viewmodel.ImWriteViewModel
 import com.goorm.kkiri.ui.mypage.viewmodel.TabItem
 
+
 @RequiresApi(Build.VERSION_CODES.O)
 class ImWriteFragment : BaseFragment<FragmentImWriteBinding>(R.layout.fragment_im_write) {
     private val viewModel: ImWriteViewModel by activityViewModels()
-    override fun setLayout() {
 
+    override fun setLayout() {
         setupViewPagerAndTabs()
+        setupTopButton()
     }
 
+    //top버튼 이벤트 추가
+    private fun setupTopButton() {
+        val fadeIn = AlphaAnimation(0f, 1f).apply { duration = 500 }
+        val fadeOut = AlphaAnimation(1f, 0f).apply { duration = 500 }
+
+        viewModel.floatButtonVisibility.observe(viewLifecycleOwner) { visible ->
+            binding.fbTopBtn.visibility = if (visible) View.VISIBLE else View.GONE
+            binding.fbTopBtn.startAnimation(if (visible) fadeIn else fadeOut)
+        }
+
+        binding.fbTopBtn.setOnClickListener {
+            viewModel.scrollToTop()
+        }
+    }
+
+    //뷰페이저에 탭 아이템 적용
     private fun setupViewPagerAndTabs() {
-        // tabItems LiveData 관찰
         viewModel.tabItems.observe(viewLifecycleOwner) { tabItems ->
             val pagerAdapter = MyPagerAdapter(this)
             binding.imWritePager.adapter = pagerAdapter
             TabLayoutMediator(binding.imWriteTab, binding.imWritePager) { tab, position ->
-                // 탭에 커스텀 뷰 적용
-                val textView = TextView(context).apply {
-                    text = tabItems[position].title
-                    textSize = 14f // 폰트 크기 설정
-                    gravity = Gravity.CENTER // 텍스트 가운데 정렬
-                    typeface = Typeface.DEFAULT_BOLD // 텍스트 굵게
-                    if (context != null) {
-                        typeface = resources.getFont(R.font.nanum_square_round_bold)
-                        // 'NanumSquareRound' 폰트 적용
-                    }
-                }
-
-                // 탭에 TextView를 설정
-                tab.customView = textView
+                setupTab(tab, tabItems[position].title)
             }.attach()
             pagerAdapter.setTabItems(tabItems)
         }
     }
 
+    //탭 바 변경시 적용 폰트 유지
+    private fun setupTab(tab: TabLayout.Tab, title: String) {
+        val textView = TextView(context).apply {
+            text = title
+            textSize = 14f
+            gravity = Gravity.CENTER
+            typeface = Typeface.DEFAULT_BOLD
+            if (context != null) {
+                typeface = resources.getFont(R.font.nanum_square_round_bold)
+            }
+        }
+        tab.customView = textView
+    }
+
+    //뷰페이저 어댑터
     private inner class MyPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
         private var tabItems: List<TabItem> = emptyList()
 
-        //탭 아이템 갈아끼우고 화면 업데이트
         fun setTabItems(tabItems: List<TabItem>) {
             this.tabItems = tabItems
             notifyDataSetChanged()
@@ -59,8 +80,6 @@ class ImWriteFragment : BaseFragment<FragmentImWriteBinding>(R.layout.fragment_i
 
         override fun getItemCount(): Int = tabItems.size
 
-        override fun createFragment(position: Int): Fragment {
-            return tabItems[position].fragment
-        }
+        override fun createFragment(position: Int): Fragment = tabItems[position].fragment
     }
 }

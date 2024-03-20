d4eb256cdb73e894dc0b4a40924d8267ee34b9c7
jiwon <pjw99032986@gmail.com>
2024-03-20 16:54:06 +0900

    refactor : tabBar 업데이트 폰트 설정

--- app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/ImWriteFragment.kt
@@ -1,12 +1,12 @@
 package com.goorm.kkiri.ui.mypage
 
+import android.graphics.Typeface
 import android.os.Build
-import android.os.Bundle
-import android.view.View
+import android.view.Gravity
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.goorm.kkiri.R
@@ -24,16 +24,26 @@ class ImWriteFragment : BaseFragment<FragmentImWriteBinding>(R.layout.fragment_i
     }
 
     private fun setupViewPagerAndTabs() {
-
         // tabItems LiveData 관찰
         viewModel.tabItems.observe(viewLifecycleOwner) { tabItems ->
-
             val pagerAdapter = MyPagerAdapter(this)
             binding.imWritePager.adapter = pagerAdapter
             TabLayoutMediator(binding.imWriteTab, binding.imWritePager) { tab, position ->
-                tab.text = tabItems[position].title
-            }.attach()
+                // 탭에 커스텀 뷰 적용
+                val textView = TextView(context).apply {
+                    text = tabItems[position].title
+                    textSize = 14f // 폰트 크기 설정
+                    gravity = Gravity.CENTER // 텍스트 가운데 정렬
+                    typeface = Typeface.DEFAULT_BOLD // 텍스트 굵게
+                    if (context != null) {
+                        typeface = resources.getFont(R.font.nanum_square_round_bold)
+                        // 'NanumSquareRound' 폰트 적용
+                    }
+                }
 
+                // 탭에 TextView를 설정
+                tab.customView = textView
+            }.attach()
             pagerAdapter.setTabItems(tabItems)
         }
     }

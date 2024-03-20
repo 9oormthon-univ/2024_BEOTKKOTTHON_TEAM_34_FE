3fd7da66491309b4e5fba3f0b5e6bfb6923019ab
jiwon <pjw99032986@gmail.com>
2024-03-21 04:58:42 +0900

    perf : 탑 버튼 효과 각각 리사이클러뷰가 속한 프레그먼트로 이전

--- app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
+++ app/src/main/java/com/goorm/kkiri/ui/mypage/HelpListFragment.kt
@@ -2,11 +2,17 @@ package com.goorm.kkiri.ui.mypage
 
 import android.content.Intent
 import android.os.Build
+import android.provider.ContactsContract.Data
+import android.view.View
+import android.view.animation.AlphaAnimation
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.goorm.kkiri.R
 import com.goorm.kkiri.base.BaseFragment
+import com.goorm.kkiri.data.local.DataSource
 import com.goorm.kkiri.databinding.FragmentHelpListBinding
 import com.goorm.kkiri.ui.mypage.adapter.MenuClickListener
 import com.goorm.kkiri.ui.mypage.adapter.MyWrittenHelpAdapter
@@ -17,7 +23,7 @@ class HelpListFragment : BaseFragment<FragmentHelpListBinding>(R.layout.fragment
     MenuClickListener {
 
     private val viewModel: ImWriteViewModel by activityViewModels()
-    private val myWrittenHelpAdapter = MyWrittenHelpAdapter(this)
+    private lateinit var helpAdapter: MyWrittenHelpAdapter
 
 
     override fun setLayout() {
@@ -27,42 +33,57 @@ class HelpListFragment : BaseFragment<FragmentHelpListBinding>(R.layout.fragment
     //어댑터 초기화
     private fun initAdapter() {
         setupRecyclerView()
-        observeViewModel()
+        setTopButton()
     }
+
     //리사이클러뷰 세팅
     private fun setupRecyclerView() {
-        binding.recyclerHelp.adapter = myWrittenHelpAdapter
+        helpAdapter = MyWrittenHelpAdapter(this)
+        helpAdapter.update(DataSource.writtenItems)
+        binding.recyclerHelp.adapter = helpAdapter
+        viewModel.helpItems.observe(viewLifecycleOwner) { items ->
+            if (items.isEmpty()) {
+                emptyListIntro(true) // 리스트가 비었을 때 안내문 보이기
+            } else {
+                emptyListIntro(false) // 리스트에 데이터가 있을 때 안내문 숨기기
+
+            }
+
+            helpAdapter.update(items)
+        }
+
     }
+    private fun emptyListIntro(state : Boolean){
+        binding.tvEmptyHelpInstructions.isVisible = state
+    }
+    private fun setTopButton(){
+        val fadeIn = AlphaAnimation(0f, 1f).apply { duration = 500 }
+        val fadeOut = AlphaAnimation(1f, 0f).apply { duration = 500 }
+        var isTop = true
 
-    //뷰 모델 옵저버, 스크롤 이벤트 장착
-    private fun observeViewModel() {
-        viewModel.itemList.observe(viewLifecycleOwner) { items ->
-            myWrittenHelpAdapter.update(items)
-        }
-
-        viewModel.scrollToTopEvent.observe(viewLifecycleOwner) {
-            scrollToTop()
-        }
-
-        binding.recyclerHelp.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        binding.recyclerHelp.addOnScrollListener(object: RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-                handleScrollStateChange(newState)
+                if (!binding.recyclerHelp.canScrollVertically(-1)
+                    && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    binding.fbHelpTopBtn.startAnimation(fadeOut)
+                    binding.fbHelpTopBtn.visibility = View.GONE
+                    isTop = true
+                } else {
+                    if(isTop) {
+                        binding.fbHelpTopBtn.visibility = View.VISIBLE
+                        binding.fbHelpTopBtn.startAnimation(fadeIn)
+                        isTop = false
+                    }
+                }
             }
         })
+
+        binding.fbHelpTopBtn.setOnClickListener {
+            binding.recyclerHelp.smoothScrollToPosition(0)
+        }
     }
 
-    //스크롤이 멈추면 버튼의 가시성 변경
-    private fun handleScrollStateChange(newState: Int) {
-        val recyclerView = binding.recyclerHelp
-        val canScrollUp = recyclerView.canScrollVertically(-1)
-        viewModel.setFloatButtonVisibility(canScrollUp || newState != RecyclerView.SCROLL_STATE_IDLE)
-    }
-
-    //스크롤을 맨 위로 올리는 함수
-    private fun scrollToTop() {
-        binding.recyclerHelp.smoothScrollToPosition(0)
-    }
 
     //메뉴를 누르면 이동
     override fun menuClickListener(position: Long) {

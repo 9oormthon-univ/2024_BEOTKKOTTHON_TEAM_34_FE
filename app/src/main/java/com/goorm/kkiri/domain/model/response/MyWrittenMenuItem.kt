40030a4577129a289b41f1fcb6a7b3b634fe5332
jiwon <pjw99032986@gmail.com>
2024-03-22 21:15:58 +0900

    refactor : MyWrittenMenuItem 수정

--- app/src/main/java/com/goorm/kkiri/domain/model/response/MyWrittenMenuItem.kt
+++ app/src/main/java/com/goorm/kkiri/domain/model/response/MyWrittenMenuItem.kt
@@ -6,13 +6,11 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
-@Parcelize // 임시 데이터 타입
 data class MyWrittenMenuItem(
-    @SerializedName("id_wt")
     val recordIdWt: Long,
     var beenCountWt: Long,
     var dateWt: LocalDate,
     var titleWt: String,
     val imgWt: Uri?,
     var expWt: String
-) : Parcelable
+)

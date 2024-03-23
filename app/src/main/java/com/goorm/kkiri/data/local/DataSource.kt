f6d452094529261b21b8a965dd94f46148d118d3
Tnalxmsk <alstnwkd990@naver.com>
2024-03-23 17:39:42 +0900

    refactor: 불필요 local DataSource 코드 삭제

--- app/src/main/java/com/goorm/kkiri/data/local/DataSource.kt
+++ app/src/main/java/com/goorm/kkiri/data/local/DataSource.kt
@@ -9,12 +9,11 @@ import com.goorm.kkiri.domain.model.response.ChatRoomItem
 import com.goorm.kkiri.domain.model.response.MyWrittenMenuItem
 import com.goorm.kkiri.domain.model.response.HelpPostInfo
 import com.goorm.kkiri.domain.model.response.MyImageItem
-import com.goorm.kkiri.domain.model.response.PostItem
+import com.goorm.kkiri.domain.model.response.BoardIndividualDto
 import java.time.LocalDate
 @RequiresApi(Build.VERSION_CODES.O)
 object DataSource {
     val uri = Uri.parse("android.resource://com.goorm.kkiri/drawable/ic_camera")
-    var postItems = mutableListOf<PostItem>()
     var writtenItems = mutableListOf<MyWrittenMenuItem>()
     var writtenItems2 = mutableListOf<MyWrittenMenuItem>()
     var pictureData = mutableListOf<MyImageItem>()
@@ -48,39 +47,6 @@ object DataSource {
     fun initPicturesData(){
         pictureData.clear()
         pictureData.add(MyImageItem(0,uri,"0"))
-    }
-    fun initHelpMePostItems() {
-        postItems.clear()
-        postItems.add(
-            PostItem(1, "코딩 알려주실분!", R.drawable.ic_app_logo, "5분전", 5, 6),
-        )
-        postItems.add(
-            PostItem(2, "토익 800점 찍고 싶어요 토익 알려주실분 구합니다", R.drawable.ic_app_logo, "10분전", 5, 6),
-        )
-        postItems.add(
-            PostItem(3, "편입 영어 도와주세요!", R.drawable.ic_app_logo, "45분전", 5, 6),
-        )
-        postItems.add(
-            PostItem(4, "피그마 기초 사용", R.drawable.ic_app_logo, "1시간전", 5, 6),
-        )
-        postItems.add(
-            PostItem(5, "안드로이드 개발 재능 나눔", R.drawable.ic_app_logo, "12시간전", 5, 6),
-        )
-        postItems.add(
-            PostItem(6, "코딩 알려주실분!", R.drawable.ic_app_logo, "17시간전", 5, 6),
-        )
-        postItems.add(
-            PostItem(7, "코딩 알려주실분!", R.drawable.ic_app_logo, "3월 18일", 5, 6),
-        )
-        postItems.add(
-            PostItem(8, "코딩 알려주실분!", R.drawable.ic_app_logo, "3월 17일", 5, 6),
-        )
-        postItems.add(
-            PostItem(9, "코딩 알려주실분!", R.drawable.ic_app_logo, "3월 16일", 5, 6),
-        )
-        postItems.add(
-            PostItem(10, "코딩 알려주실분!", R.drawable.ic_app_logo, "3월 13일", 5, 6),
-        )
     }
 
     fun getHelpPostInfo(): HelpPostInfo {
